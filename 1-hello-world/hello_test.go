77f5150b148bd1c251a69cb18161c27be24474ef
LOTEAT <zenglezhu@gmail.com>
2023-02-12 09:56:55 +0800

    hello

---
+++ 1-hello-world/hello_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func Hello() string {
+	return "Hello, world!"
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello()
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello()
+		want := "Hello, world!"
+		assertCorrectMessage(t, got, want)
+	})
+}
